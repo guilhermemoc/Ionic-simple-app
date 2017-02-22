feacbda64eb698ba27d6395f259e793e60387b93
Guilherme Martins <adm.gms@gmail.com>
2017-02-22 20:21:04 -0300

    ..

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { IonicApp, IonicModule } from 'ionic-angular';
+import { MyApp } from './app.component';
+
+import { UsersPage } from '../pages/users/users';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    UsersPage
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    UsersPage
+  ]
+})
+export class AppModule {}
